d1b53321a0bf0dbc470f428607808ffbdb0f33a9
philibertsalmon <philibert.salmon@mines-paristech.fr>
2022-01-28 11:10:03 +0100

    Update manu 1

--- Manuela.py
+++ Manuela.py
@@ -1,25 +1,24 @@
-from random import *
+import random as rd
+from turtle import pos
 
-screen = ............
-nb_lines = .........
-nb_columns = ........
-
-objets = ["B", "j"] #"*", "!", "(", "&", "o"
+def manu(salle, nb_lines, nb_columns):
+    objets = ["B", "j"] #"*", "!", "(", "&", "o"
 
 
-screen_objets = screen.copy()
-monstres = []
-potions = []
+    screen_objets = salle.copy()
+    monstres = []
+    potions = []
 
 
-for i in range(nb_lines):
-    for j in range(nb_columns):
-        if screen[i][j] == ".":
-            aleatoire = random.randint(1, 36)
-            if aleatoire == 1:
-                objet = random.choice(objets)
-                screen_objets[i,j] = objet
-                if objet == "B":
-                    monstres.append((i,j))
-                else:
-                    potions.append((i,j))
+    for i in range(nb_lines):
+        for j in range(nb_columns):
+            if salle[i][j] == ".":
+                aleatoire = rd.randint(1, 36)
+                if aleatoire == 1:
+                    objet = rd.choice(objets)
+                    screen_objets[i,j] = objet
+                    if objet == "B":
+                        monstres.append((i,j))
+                    else:
+                        potions.append((i,j))
+    return potions, monstres
